a4bba3712c0024ff36b70c899b4f413f97c0c853
Yesmin Lahoud <ymslahoud@gmail.com>
2022-10-10 17:56:15 -0300

    Exercicios if else e switch

---
+++ ExercicioIntervalo.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ExercicioIntervalo{
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+        float numero;
+
+        numero = teclado.nextFloat();
+
+        if(numero >= 0 && numero <= 25){
+            System.out.println("Intervalo [0,25]");
+        }
+        else if(numero > 25 && numero <= 50){
+            System.out.println("Intervalo (25,50]");
+        }
+        else if(numero > 50 && numero <= 75){
+            System.out.println("Intervalo (50,75]");
+        }
+        else if(numero > 75 && numero <= 100){
+            System.out.println("Intervalo (75,100]");
+        }
+        else{
+            System.out.println("Fora do intervalo");
+        }
+
+        teclado.close();
+    }
+}
